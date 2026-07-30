1b2a2cac4b0faec503c882b9e113981faf459511
dev-mahbub <mahbub.devs@gmail.com>
2026-07-30 17:35:49 +0600

    access token updated done

--- proxy.ts
+++ proxy.ts
@@ -1,23 +1,58 @@
 import { JwtPayload } from "jsonwebtoken";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import jwt from "jsonwebtoken";
+import { jwtUtils } from "./utils/jwt";
+import { cookies } from "next/headers";
+import { getRefreshToken } from "./service/refreshToken";
 
 const AUTH_ROUTES = ["/login", "/register"];
 const PUBLIC_ROUTES = ["/", "/news", "/login", "/register"];
 // This function can be marked `async` if using `await` inside
-export function proxy(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
-  //   console.log("request", request.nextUrl);
-  //   console.log("pathname", request.nextUrl.pathname);
-  const accessToken = request.cookies.get("accessToken")?.value;
-  const decodedToken = accessToken
-    ? (jwt.decode(accessToken) as JwtPayload)
+  const cookieStore = await cookies();
+
+  let accessToken = request.cookies.get("accessToken")?.value;
+  const refreshToken = request.cookies.get("refreshToken")?.value;
+
+  let decodedAccessToken = accessToken
+    ? jwtUtils.verifyToken(accessToken, process.env.JWT_ACCESS_SECRET as string)
     : null;
 
+  const decodedRefreshToken = refreshToken
+    ? jwtUtils.verifyToken(
+        refreshToken,
+        process.env.JWT_REFRESH_SECRET as string,
+      )
+    : null;
+
+  if (!decodedAccessToken?.success && decodedRefreshToken?.success) {
+    const result = await getRefreshToken();
+
+    if (result.success) {
+      const newAccessToken = result.data.accessToken;
+      cookieStore.set("accessToken", newAccessToken, {
+        httpOnly: true,
+        maxAge: 60 * 60 * 24,
+        sameSite: "lax",
+      });
+
+      accessToken = newAccessToken;
+      decodedAccessToken = jwtUtils.verifyToken(
+        accessToken!,
+        process.env.JWT_ACCESS_SECRET as string,
+      );
+    }
+  }
+
   let userRole = null;
-  if (decodedToken) {
-    userRole = decodedToken.role;
+
+  if (!decodedAccessToken?.success) {
+    cookieStore.delete("accessToken");
+  }
+
+  if (decodedAccessToken?.success && decodedAccessToken.data) {
+    userRole = (decodedAccessToken.data as JwtPayload).role;
   }
 
   if (accessToken && AUTH_ROUTES.includes(pathname)) {
